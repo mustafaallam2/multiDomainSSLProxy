5429dd2ff34b8c5297bf84166a927ba64fba2f64
mustafaallam2 <mustafaallam74@gmail.com>
2020-12-27 19:26:09 +0200

    init

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello, World!");
+})
+
+app.get("/test", (req, res) => {
+    res.send("Hello, World! from test");
 })
 
 module.exports = app;
